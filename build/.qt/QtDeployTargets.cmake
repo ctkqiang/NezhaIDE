0066ce2b4e70bf205f6e5cc57aa9d2d2252b1fa7
🇨🇳钟智强 『哪吒网络安全』 <johnmelodymel@qq.com>
2026-08-12 18:30:18 +0800

    Delete build directory

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_NezhaIDE_FILE /Users/johnmelodyme/Documents/ctkqiang/NezhaIDE/build/NezhaIDE.app/Contents/MacOS/NezhaIDE)
-set(__QT_DEPLOY_TARGET_NezhaIDE_TYPE EXECUTABLE)
--- build/CMakeFiles/NezhaIDE.dir/NezhaIDE_autogen/BTBNRJ4WLO/qrc_vectors.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/NezhaIDE.dir/NezhaIDE_autogen/BTBNRJ4WLO/qrc_vectors.cpp.o: \
-  /Users/johnmelodyme/Documents/ctkqiang/NezhaIDE/build/NezhaIDE_autogen/BTBNRJ4WLO/qrc_vectors.cpp
